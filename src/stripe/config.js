ce13dc48059646940b041ada31bc0bacf66650fd
Maksym7415 <popovmaksim7415@gmail.com>
2021-02-16 09:52:04 +0200

    changed edpoint url

--- src/stripe/config.js
+++ src/stripe/config.js
@@ -1,6 +1,6 @@
 const config = {
   stripePk: 'pk_test_51HGjVfCcIG0MPJSGSyEcBP3PyICKdRKpXMC1TP4r08nXQ6diqfdIN4Uw3O5c1vdgEqc7deyecbDYde46HdRMdVtt00trU2DOSP',
-  clientSecretUrl: 'https://stripe-example.hopto.org/sandbox/client-secret',
+  clientSecretUrl: 'https://stripe-example.hopto.org/api/client-secret',
   // clientSecretUrl: 'http://localhost:8000/sandbox/client-secret'
 }
 
