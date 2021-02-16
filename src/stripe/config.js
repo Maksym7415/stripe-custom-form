9d1f9dc636657bf48ecef925fc5d0e5acafb383b
Maksym7415 <popovmaksim7415@gmail.com>
2021-02-16 09:37:59 +0200

    fixed config file

--- src/stripe/config.js
+++ src/stripe/config.js
@@ -1,7 +1,7 @@
 const config = {
   stripePk: 'pk_test_51HGjVfCcIG0MPJSGSyEcBP3PyICKdRKpXMC1TP4r08nXQ6diqfdIN4Uw3O5c1vdgEqc7deyecbDYde46HdRMdVtt00trU2DOSP',
-  // clientSecretUrl: 'https://eat-beat.hopto.org/sandbox/client-secret',
-  clientSecretUrl: 'http://localhost:8000/sandbox/client-secret'
+  clientSecretUrl: 'https://stripe-example.hopto.org/sandbox/client-secret',
+  // clientSecretUrl: 'http://localhost:8000/sandbox/client-secret'
 }
 
 export default config;
