a6389f6c845505f04c2682cad5e5af2850d102c0
Maksym7415 <popovmaksim7415@gmail.com>
2021-03-23 09:13:31 +0200

    changed api key

--- src/stripe/config.js
+++ src/stripe/config.js
@@ -1,5 +1,5 @@
 const config = {
-  stripePk: 'pk_test_51HGjVfCcIG0MPJSGSyEcBP3PyICKdRKpXMC1TP4r08nXQ6diqfdIN4Uw3O5c1vdgEqc7deyecbDYde46HdRMdVtt00trU2DOSP',
+  stripePk: 'pk_test_51HGjVfCcIG0MPJSGFsxlYkGc3Cj6ZMDgdDW5nyDiqOWCQsMEMlaZMvOzvM22hYYjX6Qgawocr7EQHLETyO0r5Hj100yp3OBL3q',
   clientSecretUrl: 'https://stripe-example.hopto.org/api/client-secret',
   // clientSecretUrl: 'http://localhost:8000/sandbox/client-secret'
 }
